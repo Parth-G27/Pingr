ea75a109f7cea9aa782b94544fff922c77bbb5c2
Parth <parthbidari13@gmail.com>
2024-12-02 20:08:28 +0530

    going into prisma

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,9 @@
 import React, { ReactNode } from "react";
 
-function Layout({ children }: { children: ReactNode }) {
+function Layout({ children } : { children: ReactNode }) {
   return (
     <div className="flex min-h-screen w-full flex-col items-center dark:bg-neutral-950">
-      {/* Wrapper for content with responsive width */}
-      <div className="flex-grow w-full max-w-[90%] sm:max-w-[1080px] px-4 py-8 justify-center pt-[40px]">
+      <div className="flex-grow w-full max-w-[90%] sm:max-w-[1280px] px-4 py-8 justify-center pt-[40px]">
         {children}
       </div>
     </div>
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -19,14 +19,16 @@ async function WelcomeMessage() {
     return <div>No user found</div>;
   }
   return (
-    <div className="flex min-h-screen w-full flex-col items-center p-2 pb-20 gap-16 sm:p-8 dark:bg-neutral-950">
-      <h3 className="flex flex-col sm:flex-row items-start sm:items-center text-3xl sm:text-5xl font-bold bg-gradient-to-r from-red-600 via-red-800 to-red-600 dark:from-red-800 dark:via-red-600 dark:to-red-800 bg-clip-text text-transparent px-2">
-        <span>Welcome,</span>
-        <span className=" text-4xl sm:text-5xl bg-gradient-to-r from-orange-600 via-red-500 to-orange-600 dark:from-orange-600 dark:via-red-600 dark:to-orange-600 bg-clip-text text-transparent mt-2 sm:mt-0 sm:ml-3">
-          {user?.firstName} {user?.lastName}
-        </span>
-      </h3>
-    </div>
+    <>
+      <div className="flex min-h-screen w-full flex-col items-center p-2 pb-10 gap-16 sm:p-8 dark:bg-neutral-950">
+        <h3 className="flex flex-col sm:flex-row items-start text-3xl sm:text-5xl font-bold bg-gradient-to-r from-red-600 via-red-800 to-red-600 dark:from-red-800 dark:via-red-600 dark:to-red-800 bg-clip-text text-transparent px-1 w-full">
+          <span>Welcome,</span>
+          <span className="text-4xl sm:text-5xl bg-gradient-to-r from-orange-600 via-red-500 to-orange-600 dark:from-orange-600 dark:via-red-600 dark:to-orange-600 bg-clip-text text-transparent sm:ml-3">
+            {user?.firstName} {user?.lastName}
+          </span>
+        </h3>
+      </div>
+    </>
   );
 }
 
