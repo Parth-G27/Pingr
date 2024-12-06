a42dd463748ffe097a83a817dfd8ea3a9059d337
Parth <parthbidari13@gmail.com>
2024-12-07 01:37:20 +0530

    zod done & CollectionSheet UI DONE

--- components/CreateCollectionSheet.tsx
+++ components/CreateCollectionSheet.tsx
@@ -1,57 +1,140 @@
 import { useForm } from "react-hook-form";
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "./ui/sheet";
-import { createCollectionSchema, createCollectionSchemaType } from "../schema/CreateCollection";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "./ui/sheet";
+import {
+  createCollectionSchema,
+  createCollectionSchemaType,
+} from "../schema/CreateCollection";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
 import { Input } from "./ui/input";
-import  {zodResolver}  from "@hookform/resolvers/zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { CollectionColor, CollectionColors } from "../lib/colorConstants";
+import { cn } from "../lib/utils";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
 
 interface Props {
-    keepCount: number;
-    countFunction: (count: number) => void;
-    open: boolean;
-    onOpenChange: (open: boolean) => void;
-  }
+  keepCount: number;
+  countFunction: (count: number) => void;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
 
 function CreateCollectionSheet({ open, onOpenChange }: Props) {
     const form = useForm<createCollectionSchemaType>({
         resolver: zodResolver(createCollectionSchema),
-        defaultValues: {},
+        defaultValues: {
+          name: "", 
+          color: "", 
+        },
       });
       
-      const onSubmit = (data: createCollectionSchemaType) => {
-        console.log("SUBMITTED", data);
-      };
-  return (
-    <Sheet open={open} onOpenChange={onOpenChange}>
-  <SheetContent>
-    <SheetHeader className="flex flex-col items-center">
-      <SheetTitle>Add new collection</SheetTitle>
-      <SheetDescription>Collections are a way to group your tasks</SheetDescription>
-    </SheetHeader>
 
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Name</FormLabel>
-              <FormControl>
-                <Input placeholder="Personal" {...field} />
-              </FormControl>
-              <FormDescription>Collection name</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        {/* <FormField control={form.control} name="color" /> */}
-      </form>
-    </Form>
-  </SheetContent>
-</Sheet>
+  const onSubmit = (data: createCollectionSchemaType) => {
+    console.log("SUBMITTED", data);
+  };
+
+  const openChangeWrapper = (open:boolean) => {
+    form.reset();
+    onOpenChange(open);
+  }
+  return (
+    <Sheet open={open} onOpenChange={openChangeWrapper}>
+      <SheetContent>
+        <SheetHeader className="flex flex-col items-center">
+          <SheetTitle>Add new collection</SheetTitle>
+          <SheetDescription>
+            Collections are a way to group your tasks
+          </SheetDescription>
+        </SheetHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 flex flex-col">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Personal" {...field} />
+                  </FormControl>
+                  <FormDescription>Collection name</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="color"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Color</FormLabel>
+                  <FormControl>
+                    <Select onValueChange={(color) => field.onChange(color)}>
+                      <SelectTrigger
+                        className={cn(
+                          "w-full h-8 text-white",
+                          CollectionColors[field.value as CollectionColor]
+                        )}
+                      >
+                        <SelectValue
+                          placeholder="Color"
+                          className="w-full h-8"
+                        />
+                      </SelectTrigger>
+                      <SelectContent className="w-full">
+                        {Object.keys(CollectionColors).map((color) => (
+                          <SelectItem
+                            key={color}
+                            value={color}
+                            className={cn(
+                              "w-full h-8 rounded-md my-1 text-white focus:text-white focus:font-bold focus:ring-2 ring-neutral-600 focus:ring-inset dark:focus:ring-white focus:px-8",
+                              CollectionColors[color as CollectionColor]
+                            )}
+                          >
+                            {color}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormDescription>
+                    Select a Color for your Collection{" "}
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+        <div className="flex flex-col gap-3 mt-4">
+          <Separator />
+          <Button onClick={form.handleSubmit(onSubmit)}>Confirm</Button>
+        </div>
+      </SheetContent>
+    </Sheet>
   );
 }
 
 export default CreateCollectionSheet;
-
